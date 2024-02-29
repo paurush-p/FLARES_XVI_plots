647ee13f5739634c823e52395329aadff21fb4a4
Paurush Punyasheel <57411942+Paurush01@users.noreply.github.com>
2024-02-29 15:26:58 +0100

    Delete SFR_data_read.py

--- SFR_data_read.py
+++
@@ -1,7 +0,0 @@
-import h5py
-import numpy
-f = h5py.File("C:/Users/pauru/Documents/SKIRT/flares_00/flares_skirt_outputs.hdf5",'r')
-data=f['00/007_z008p000/Galaxy/SFR']
-SFR=numpy.array(data)
-numpy.savetxt("C:/Users/pauru/Documents/SKIRT/flares_00/SFR.txt", SFR)
-print("Done")
