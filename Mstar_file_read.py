65c1a84e3d536565ba2ff91c87e98bf11c6cdee3
Paurush Punyasheel <57411942+Paurush01@users.noreply.github.com>
2024-02-29 15:26:51 +0100

    Delete Mstar_file_read.py

--- Mstar_file_read.py
+++
@@ -1,9 +0,0 @@
-import h5py
-import numpy
-f = h5py.File("C:/Users/pauru/Documents/SKIRT/flares_00/flares_skirt_outputs.hdf5",'r')
-data=f['00/007_z008p000/Galaxy/Mstar']
-Mass=numpy.array(data)
-
-Mass= [abc*(10**10) for abc in Mass]
-numpy.savetxt("C:/Users/pauru/Documents/SKIRT/flares_00/Mass.txt", Mass)
-print("Done")
